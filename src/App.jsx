edd0477e49b30ac382a48a5d8b7647254c2e544e
Quantasteek <139091681+Quantasteek@users.noreply.github.com>
2025-08-06 22:36:20 +0530

    project section update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from './sections/Hero'
 import ShowcaseSection from './sections/ShowcaseSection'
+import Navbar from './components/Navbar'
+import FeatureCards from './sections/FeatureCards'
 
 const App = () => {
   return (
     <>
+    <Navbar/>
     <Hero/>
     <ShowcaseSection/>
+    <FeatureCards/>
     </>
   )
 }
